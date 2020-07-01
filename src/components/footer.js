bb3ba89fccae71cdba34da48f341a14abf10e164
Adetunji Tejumade <tejumadeadetunji@gmail.com>
2020-07-01 01:32:19 +0100

    fix link in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,10 +5,10 @@ class Footer extends React.Component {
         return (
             React.createElement('div', { className: 'footer' },
                 React.createElement('div', { className: 'social-links text-center text-white' },
-                    React.createElement('a', {href:"https://twitter.com/@tejumade__/"},
+                    React.createElement('a', {href:"https://twitter.com/Tejumade_"},
                         React.createElement('i', { className: 'fa fa-twitter mr-8' }),
                     ),
-                    React.createElement('a', {href:"https://linkedin.com/in/tejumade-adetunji"},
+                    React.createElement('a', {href:"https://www.linkedin.com/in/tejumade-adetunji-b1828a173"},
                         React.createElement('i', { className: 'fa fa-linkedin mr-8' }),
                     ),
                     React.createElement('a', {href:"https://github.com/AdetunjiTejumade"},
