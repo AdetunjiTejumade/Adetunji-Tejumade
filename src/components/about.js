fe2dfc9a44864ea75aa2fba990312ae8309bf4d1
AdetunjiTejumade <tejumadeadetunji@gmail.com>
2021-06-03 22:22:26 +0900

    update about page

--- src/components/about.js
+++ src/components/about.js
@@ -18,9 +18,14 @@ function About() {
             {/* TODO update about */}
           </p>
           <p>
-            My first experience with programming was when I was 13, i was
+            My first experience with programming was when I was 13, I was
             fascinated with mobile games like{" "}
-            <a href="https://en.wikipedia.org/wiki/Temple_Run" target="_blank" rel="noopener noreferrer" className="text-orange-500 hover:underline">
+            <a
+              href="https://en.wikipedia.org/wiki/Temple_Run"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-orange-500 hover:underline"
+            >
               Temple run
             </a>{" "}
             and I decided I wanted to build computer games. A family friend
@@ -28,21 +33,52 @@ function About() {
           </p>
           <p>
             Years later, I decided to try learning Html and Css, which seem to
-            click, then I started learning javascript. It felt really cool being able to create websites.
+            click, then I started learning javascript. It felt really cool being
+            able to create websites.
           </p>
           <p>
             Since then I have been improving my skills. I attended a boot camp
             after secondary school where I learnt Python and C#. And in 2020 i
             was awarded a scholarship to study to full-stack development at{" "}
-            <a href="https://openclassrooms.com/en/" target="_blank" rel="noopener noreferrer" className="text-orange-500 hover:underline">
+            <a
+              href="https://openclassrooms.com/en/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-orange-500 hover:underline"
+            >
               Openclassrooms
             </a>{" "}
             for 12 months by Facebook due to my performance in the facebook
             developer circles training.
           </p>
           <p>
-            I'm currently seeking a role where I can grow and use my skills to help a
-            company achieve their goals.
+            I'm currently seeking a role where I can grow and use my skills to
+            help a company achieve their goals.
+          </p>
+          <p className="mb-0">I currently work with</p>
+          <ul className="text-orange-500 font-bold mb-2">
+            <li>Python</li>
+            <li>Django</li>
+            <li>Ruby</li>
+            <li>Ruby on Rails</li>
+            <li>Javascript</li>
+            <li>React</li>
+          </ul>
+          <p>
+            I'm open to meeting new people, you can connect with me via{" "}
+            <a
+              className="text-orange-500 hover:underline"
+              href="https://www.linkedin.com/in/tejumade-adetunji"
+            >
+              Linkedin
+            </a>{" "}
+            or{" "}
+            <a
+              className="text-orange-500 hover:underline"
+              href="mailto:tejumadeadetunji@gmail.com"
+            >
+              tejumadeadetunji@gmail.com
+            </a>
           </p>
         </div>
       </div>
